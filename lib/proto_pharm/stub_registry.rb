1af45acbdc615e02edfeac01a24b39b476fed8d6
Allen Rettberg <allen.rettberg@freshly.com>
2020-11-29 21:56:51 -0800

    add all_requests_matching

--- lib/proto_pharm/stub_registry.rb
+++ lib/proto_pharm/stub_registry.rb
@@ -20,6 +20,13 @@ module ProtoPharm
     end
 
     # @param path [String]
+    # @param request [Object] Optional; specify a request object to match against. Default: nil.
+    # @return [Array<ProtoPharm::RequestStub>] Array of all matching request stubs, if any. See {RequestPattern#match?} for matching logic.
+    def all_requests_matching(path, request = nil)
+      request_stubs[path]&.select { |stub| stub.match?(path, request) } || []
+    end
+
+    # @param path [String]
     # @param request [Object]
     # @return [ProtoPharm::RequestStub] RequestStub matching the given path/request, if found
     def find_request_matching(path, request)
--- spec/proto_pharm/stub_registry_spec.rb
+++ spec/proto_pharm/stub_registry_spec.rb
@@ -34,6 +34,43 @@ RSpec.describe ProtoPharm::StubRegistry do
     end
   end
 
+  describe "#all_requests_matching" do
+    subject { registry.all_requests_matching(path, request) }
+
+    context "when nothing is stubbed for the path" do
+      it { is_expected.to eq([]) }
+    end
+
+    context "when something is stubbed for the path" do
+      before { registry.register_request_stub(request_stub) }
+
+      it { is_expected.to eq([ request_stub ]) }
+
+      context "when multiple things are stubbed for the path" do
+        before { registry.register_request_stub(another_request_stub) }
+
+        context "when the second stub has no request specified" do
+          let(:another_request_stub) { ProtoPharm::RequestStub.new(path).to_return(response) }
+
+          it { is_expected.to match_array([ request_stub, another_request_stub ]) }
+        end
+
+        context "when the second stubbed request matches the initial one" do
+          let(:another_request_stub) { ProtoPharm::RequestStub.new(path).with(request).to_return(response) }
+
+          it { is_expected.to match_array([ request_stub, another_request_stub ]) }
+        end
+
+        context "when the second stubbed request differs from the initial one" do
+          let(:another_request) { double }
+          let(:another_request_stub) { ProtoPharm::RequestStub.new(path).with(another_request).to_return(response) }
+
+          it { is_expected.to match_array([ request_stub ]) }
+        end
+      end
+    end
+  end
+
   describe "#find_request_matching" do
     subject { registry.find_request_matching(path, request) }
 
@@ -44,16 +81,33 @@ RSpec.describe ProtoPharm::StubRegistry do
     context "when something is stubbed for the path" do
       before { registry.register_request_stub(request_stub) }
 
-      it { is_expected.to eq request_stub }
+      context "when a request is specified on the stub" do
+        it { is_expected.to eq request_stub }
+      end
+
+      context "when a request is not specified on the stub" do
+        let(:request_stub) { ProtoPharm::RequestStub.new(path).to_return(response) }
+
+        it { is_expected.to eq request_stub }
+      end
 
       context "when multiple things are stubbed for the path" do
-        let(:another_request_stub) { ProtoPharm::RequestStub.new(path).with(request).to_return(response) }
-
         before { registry.register_request_stub(another_request_stub) }
 
-        it { is_expected.to eq another_request_stub}
+        context "when the second stubbed request matches the initial one" do
+          let(:another_request_stub) { ProtoPharm::RequestStub.new(path).with(request).to_return(response) }
 
-        it { is_expected.not_to eq request_stub }
+          it { is_expected.to eq another_request_stub}
+          it { is_expected.not_to eq request_stub }
+        end
+
+        context "when the second stubbed request differs from the initial one" do
+          let(:another_request) { double }
+          let(:another_request_stub) { ProtoPharm::RequestStub.new(path).with(another_request).to_return(response) }
+
+          it { is_expected.not_to eq another_request_stub}
+          it { is_expected.to eq request_stub }
+        end
       end
     end
   end
