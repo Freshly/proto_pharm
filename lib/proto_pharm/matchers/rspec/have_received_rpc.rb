daf6dc114cfb786ddf463ab9c37b3b598544b44b
Allen Rettberg <allen.rettberg@freshly.com>
2020-11-30 18:53:13 -0800

    add have_received_rpc.with

--- lib/proto_pharm/matchers/rspec/have_received_rpc.rb
+++ lib/proto_pharm/matchers/rspec/have_received_rpc.rb
@@ -5,28 +5,40 @@ module ProtoPharm
     module RSpec
       extend ::RSpec::Matchers::DSL
 
-      matcher :have_received_rpc do |endpoint_token| # rubocop:disable Metrics/BlockLength
+      matcher :have_received_rpc do |endpoint_reference| # rubocop:disable Metrics/BlockLength
         include Introspection
 
-        attr_reader :service_token, :endpoint_token, :expected_proto, :expected_kwargs
+        attr_reader :service_reference, :endpoint_reference, :expected_proto, :expected_kwargs
 
+        description { "receive rpc #{endpoint_reference.inspect}#{with_parameters_description}" }
 
-        match do |service_token|
-          @service_token = service_token
-          @endpoint_token = endpoint_token
+        match do |service_reference|
+          @service_reference = service_reference
+          @endpoint_reference = endpoint_reference
 
           raise RpcNotStubbedError, "RPC '#{grpc_path}' has not been stubbed. Stub it with stub_grpc_action before asserting." unless matching_request_stubs.any?
 
-          expect(received_request_stubs.size).to be > 0
+          if expected_request_proto.blank?
+            expect(received_request_stubs.size).to be > 0
+          else
+            expect(received_request_stubs.flat_map(&:received_requests)).to include expected_request_proto
+          end
         end
 
+        chain :with do |expected_proto = nil, **expected_kwargs|
+          raise ArgumentError, "assert only with proto or keyword arguments, not both" if expected_proto.present? && expected_kwargs.present?
+          raise ArgumentError, "cannot assert expected arguments multiple times in the same expectation" if (@expected_proto || @expected_kwargs).present?
+
+          @expected_proto = expected_proto
+          @expected_kwargs = expected_kwargs
+        end
 
         private
 
-        delegate :grpc_path, :input_type, :output_type, to: :endpoint
+        delegate :grpc_path, :input_type, to: :endpoint
 
         def endpoint
-          @endpoint ||= inspect_rpc(service_token, endpoint_token)
+          @endpoint ||= inspect_rpc(service_reference, endpoint_reference)
         end
 
         def received_request_stubs
@@ -34,11 +46,25 @@ module ProtoPharm
         end
 
         def matching_request_stubs
-          @matching_request_stubs ||= ProtoPharm.stub_registry.all_requests_matching(*[ grpc_path, expected_request_proto ].compact)
+          @matching_request_stubs ||= ProtoPharm.stub_registry.all_requests_matching(grpc_path, expected_request_proto)
         end
 
         def expected_request_proto
           return if expected_proto.nil? && expected_kwargs.nil?
+
+          ensure_valid_proto_type!(expected_proto)
+
+          @expected_request_proto ||= expected_proto.presence || input_type.new(**expected_kwargs)
+        end
+
+        def ensure_valid_proto_type!(expected_proto)
+          raise InvalidProtoType, "Invalid proto type #{expected_proto.class} for #{grpc_path}, expected #{input_type}" if expected_proto.present? && expected_proto.class != input_type
+        end
+
+        def with_parameters_description
+          return if expected_request_proto.blank?
+
+          " with request #{expected_request_proto.inspect}"
         end
       end
     end
--- spec/proto_pharm/matchers/rspec/have_received_rpc_spec.rb
+++ spec/proto_pharm/matchers/rspec/have_received_rpc_spec.rb
@@ -3,17 +3,17 @@
 require "proto_pharm/rspec"
 
 RSpec.describe ProtoPharm::Matchers::RSpec do
+  subject(:service) { Hello::Hello }
+
+  let(:client) { HelloClient.new }
+  let(:expected_message) { "Hi!" }
+  let(:endpoint) { :hello }
+
+  let(:request_proto_class) { service::Service.rpc_descs[endpoint.to_s.camelize.to_sym].input }
+  let(:response) { {} }
+
   describe "#have_received_rpc" do
     subject(:assertion) { expect(service).to have_received_rpc(endpoint) }
-
-    let(:client) { HelloClient.new }
-    let(:message) { "Hi!" }
-
-    let(:service) { Hello::Hello }
-    let(:endpoint) { :hello }
-
-    let(:request) { {} }
-    let(:response) { {} }
 
     context "when endpoint is not stubbed" do
       it "raises" do
@@ -31,9 +31,70 @@ RSpec.describe ProtoPharm::Matchers::RSpec do
       end
 
       context "when the rpc has been received" do
-        before { client.send_message(message) }
+        before { client.send_message(expected_message) }
 
         it { is_expected.to have_received_rpc(endpoint) }
+      end
+    end
+  end
+
+  describe "#with" do
+    let(:input_proto) { request_proto_class.new(input_hash_parameters) }
+    let(:input_hash_parameters) { { msg: expected_message } }
+
+    shared_context "with proto parameter" do
+      subject(:assertion) { expect(service).to have_received_rpc(endpoint).with(input_proto) }
+    end
+
+    shared_context "with hash parameter" do
+      subject(:assertion) { expect(service).to have_received_rpc(endpoint).with(input_hash_parameters) }
+    end
+
+    shared_context "with kwargs" do
+      subject(:assertion) { expect(service).to have_received_rpc(endpoint).with(**input_hash_parameters) }
+    end
+
+    before { stub_grpc_action(service, endpoint).to_return(response) }
+
+    context "when called with proto and hash parameters" do
+      subject(:assertion) { expect(service).to have_received_rpc(endpoint).with(input_proto, **input_hash_parameters) }
+
+      it "raises" do
+        expect { assertion }.to raise_error ArgumentError, "assert only with proto or keyword arguments, not both"
+      end
+    end
+
+    context "when called multiple times" do
+      subject(:assertion) { expect(service).to have_received_rpc(endpoint).with(input_proto).with(**input_hash_parameters) }
+
+      it "raises" do
+        expect { assertion }.to raise_error ArgumentError, "cannot assert expected arguments multiple times in the same expectation"
+      end
+    end
+
+    context "when the rpc has not been received" do
+      it { is_expected.not_to have_received_rpc(endpoint).with(input_proto) }
+    end
+
+    context "when the rpc has been received" do
+      before { client.send_message(sent_message) }
+
+      context "when the rpc has been received with the correct parameters" do
+        let(:sent_message) { expected_message }
+
+        it { is_expected.to have_received_rpc(endpoint).with(input_proto) }
+
+        context "when rpc is also called with different parameters" do
+          before { client.send_message("Ahoy!") }
+
+          it { is_expected.to have_received_rpc(endpoint).with(input_proto) }
+        end
+      end
+
+      context "when the rpc has been received with different parameters" do
+        let(:sent_message) { "Hello!" }
+
+        it { is_expected.not_to have_received_rpc(endpoint).with(input_proto) }
       end
     end
   end
