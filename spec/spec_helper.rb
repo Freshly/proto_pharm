b0451c0d1de767d91f2d8b693c1264d444ea6faf
Allen Rettberg <allen.rettberg@freshly.com>
2020-12-21 21:54:16 -0800

    do some apostrophes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
-require 'pry'
-require 'faker'
-require 'rspice'
+require "bundler/setup"
+require "pry"
+require "faker"
+require "rspice"
 
 require "proto_pharm"
 require "proto_pharm/rspec"
@@ -14,7 +14,7 @@ require_relative "examples/hello/hello_client"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
+  config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
